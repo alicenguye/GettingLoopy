6d249ed04ec8336c93bdb0619b29826aad9e52d0
Alice Nguyen <125220059+alicenguye@users.noreply.github.com>
2023-02-27 20:43:56 -0500

    Update Question5.java

--- src/Question5.java
+++ src/Question5.java
@@ -5,7 +5,7 @@ public class Question5 {
         int j;
 //start looping
         for (i = 1; i <= 5; i++) {
-            for (j = 2; j <= i; j++)
+            for (j = 1; j <= i; j++)
             {
                 System.out.printf("*");
             }
